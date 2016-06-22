ea1b7a076aff42d4bfe3e8191ccdcea7ee96dcec
John Eckstein <jeckste@gmail.com>
2016-06-22 11:35:59 -0400

    work on bam

---
+++ filetypeHandlers/org.lorainelab.igb.filehandler.bam/src/main/java/org/lorainelab/igb/filehandler/bam/AlignmentBlock.java
@@ -0,0 +1,35 @@
+package org.lorainelab.igb.filehandler.bam;
+
+import com.google.common.collect.Range;
+
+/**
+ *
+ * @author dcnorris
+ */
+public class AlignmentBlock {
+
+    private final Range<Integer> range;
+    private final AlignmentType alignmentType;
+
+    public enum AlignmentType {
+        INSERTION,
+        DELETION,
+        MATCH,
+        GAP,
+        PADDING
+    }
+
+    public AlignmentBlock(Range<Integer> range, AlignmentType alignmentType) {
+        this.range = range;
+        this.alignmentType = alignmentType;
+    }
+
+    public Range<Integer> getRange() {
+        return range;
+    }
+
+    public AlignmentType getAlignmentType() {
+        return alignmentType;
+    }
+
+}
