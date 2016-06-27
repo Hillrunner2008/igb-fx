beeea8d1bd60bdacc502f6652278f3dfb5cf3081
John Eckstein <jeckste@gmail.com>
2016-06-27 18:25:24 -0400

    work

--- filetypeHandlers/org.lorainelab.igb.filehandler.bam/src/test/java/org/lorainelab/igb/filehandler/bam/BamParserTest.java
+++ filetypeHandlers/org.lorainelab.igb.filehandler.bam/src/test/java/org/lorainelab/igb/filehandler/bam/BamParserTest.java
@@ -5,6 +5,8 @@
  */
 package org.lorainelab.igb.filehandler.bam;
 
+import htsjdk.samtools.SAMRecord;
+import htsjdk.samtools.SAMRecordIterator;
 import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.SamInputResource;
 import htsjdk.samtools.SamReader;
@@ -15,8 +17,8 @@ import htsjdk.samtools.seekablestream.SeekableFileStream;
 import htsjdk.samtools.util.CloserUtil;
 import java.io.File;
 import java.util.List;
+import java.util.Optional;
 import org.slf4j.LoggerFactory;
-import org.testng.annotations.Test;
 
 /**
  *
@@ -51,5 +53,34 @@ public class BamParserTest {
         } catch (Exception ex) {
             LOG.error(ex.getMessage(), ex);
         }
+    }
+    
+    public static void testGetRecordsBySequence(String chromosomeId) {
+        try (SeekableBufferedStream bamSeekableStream = new SeekableBufferedStream(
+                    new SeekableFileStream(new File("/home/jeckstei/Downloads/cold_control.mm.bam")));
+                    SeekableBufferedStream indexSeekableStream = new SeekableBufferedStream(
+                            new SeekableFileStream(new File("/home/jeckstei/Downloads/cold_control.mm.bam.bai")));) {
+                SamReader reader = SamReaderFactory.make()
+                        .validationStringency(ValidationStringency.SILENT).open(SamInputResource.of(bamSeekableStream));
+                final List<SAMSequenceRecord> seqRecords = reader.getFileHeader().getSequenceDictionary().getSequences();
+                Optional<SAMSequenceRecord> sequence = getSequenceByName(chromosomeId, seqRecords);
+                SAMSequenceRecord record = sequence.get();
+                int start = record.getSequenceIndex();
+                int end = start + record.getSequenceLength();
+                
+                try (SAMRecordIterator iter = reader.query(record.getSequenceName(), start, end, true)) {
+                    while (iter.hasNext()) {
+                        SAMRecord samRecord = iter.next();
+                        annotations.add(new BamFeature(samRecord));
+                    }
+                }
+                CloserUtil.close(reader);
+            } catch (Exception ex) {
+                LOG.error(ex.getMessage(), ex);
+            }
+    }
+    
+    private static Optional<SAMSequenceRecord> getSequenceByName(String sequence, List<SAMSequenceRecord> seqRecords) {
+        return seqRecords.stream().filter(r -> sequence.equals(r.getSequenceName())).findFirst();
     }
 }
