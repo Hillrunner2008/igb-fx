7144653e7d40a011924f00255ef6b17f2b18b38d
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-07-06 15:07:23 -0400

    renamed preferencemanager to remove camel case

--- preferences/org.lorainelab.igb.preferencemanager.api/src/main/java/org/lorainelab/igb/preferencemanager/api/PreferencesTabProvider.java
+++ preferences/org.lorainelab.igb.preferencemanager.api/src/main/java/org/lorainelab/igb/preferencemanager/api/PreferencesTabProvider.java
@@ -8,11 +8,22 @@ package org.lorainelab.igb.preferencemanager.api;
 import javafx.scene.control.Tab;
 
 /**
+ * This interface allows implementors to provide tabs in IGB preferences.
  *
  * @author Devdatta Kulkarni
  */
 public interface PreferencesTabProvider {
-    
+  
+    /**
+     *
+     * This method will be called when an App is initialized.
+     *
+     * @return a tab that has to be added to preferences.
+     *
+     */
      Tab getPreferencesTab();
+     /**
+     * @return the weight of tab. Tabs will be sorted on this weight
+     */
      int getTabWeight();
 }
