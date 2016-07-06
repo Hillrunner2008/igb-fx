ac7915abd76464332460b500459d3c833933a3bf
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-07-06 11:09:58 -0400

    added file preferencetabmanager

---
+++ preferences/org.lorainelab.igb.preferencemanager.api/src/main/java/org/lorainelab/igb/preferencemanager/api/PreferencesTabManager.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lorainelab.igb.preferencemanager.api;
+
+import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Devdatta Kulkarni
+ */
+@Component(immediate = true, provide = PreferencesTabManager.class)
+public class PreferencesTabManager {
+
+    private static final Logger LOG = LoggerFactory.getLogger(PreferencesTabManager.class);
+    private TabPane pane;
+
+    public PreferencesTabManager() {
+        System.out.println(PreferencesTabManager.class+" init comp");
+        pane = new TabPane();
+        setAnchorPaneConstraints(pane);
+        pane.getTabs().add(new Tab("Hello Tab"));
+    }
+
+
+    @Reference(optional = true, multiple = true, unbind = "removeTab", dynamic = true)
+    public void addTab(PreferencesTabProvider tabProvider) {
+        tabProvider.getPreferencesTab().setClosable(false);
+        System.out.println("tab closing set to false");
+        pane.getTabs().add(tabProvider.getPreferencesTab());
+        System.out.println("isclosable: "+tabProvider.getPreferencesTab().isClosable());
+    }
+
+    public void removeTab(PreferencesTabProvider tabProvider) {
+        pane.getTabs().remove(tabProvider.getPreferencesTab());
+    }
+
+    
+    private void setAnchorPaneConstraints(TabPane pane) {
+        double anchor = 0;
+        AnchorPane.setBottomAnchor(pane, anchor);
+        AnchorPane.setTopAnchor(pane, anchor);
+        AnchorPane.setRightAnchor(pane, anchor);
+        AnchorPane.setLeftAnchor(pane, anchor);
+    }
+
+}
