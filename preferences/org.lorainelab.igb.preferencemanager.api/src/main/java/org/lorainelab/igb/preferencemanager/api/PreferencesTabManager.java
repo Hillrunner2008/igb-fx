5367c95acdb04fafc0a3d69c1dc59b8b844d50fd
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-07-13 08:57:59 -0400

    added sorting for tabs

--- preferences/org.lorainelab.igb.preferencemanager.api/src/main/java/org/lorainelab/igb/preferencemanager/api/PreferencesTabManager.java
+++ preferences/org.lorainelab.igb.preferencemanager.api/src/main/java/org/lorainelab/igb/preferencemanager/api/PreferencesTabManager.java
@@ -7,6 +7,11 @@ package org.lorainelab.igb.preferencemanager.api;
 
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Reference;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Tab;
@@ -24,24 +29,30 @@ import org.slf4j.LoggerFactory;
 public class PreferencesTabManager {
 
     private static final Logger LOG = LoggerFactory.getLogger(PreferencesTabManager.class);
+    private SortedSet<PreferencesTabProvider> tabs;
     private TabPane pane;
 
     public PreferencesTabManager() {
         pane = new TabPane();
+        tabs = new TreeSet<PreferencesTabProvider>((x,y)-> y.getTabWeight() - x.getTabWeight());
         setAnchorPaneConstraints(pane);
     }
 
 
     @Reference(optional = true, multiple = true, unbind = "removeTab", dynamic = true)
     public void addTab(PreferencesTabProvider tabProvider) {
-        pane.getTabs().add(tabProvider.getPreferencesTab());
+        //pane.getTabs().add(tabProvider.getPreferencesTab());
+        tabs.add(tabProvider);
     }
 
     public void removeTab(PreferencesTabProvider tabProvider) {
-        pane.getTabs().remove(tabProvider.getPreferencesTab());
+        //pane.getTabs().remove(tabProvider.getPreferencesTab());
+        tabs.remove(tabProvider);
     }
 
     public TabPane getPreferencesTabPane(){
+        pane.getTabs().clear();
+        pane.getTabs().addAll(tabs.stream().map(tab -> tab.getPreferencesTab()).collect(Collectors.toList()));
         return pane;
     }
     
