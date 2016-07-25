8619cbdf5da288376437d382721484a30db9e962
David Norris <Hillrunner2008@gmail.com>
2016-07-25 11:17:28 -0400

    Added utility class for runAndWait feature

---
+++ org.lorainelab.igb.visualization/src/main/java/org/lorainelab/igb/visualization/util/FXUtilities.java
@@ -0,0 +1,19 @@
+package org.lorainelab.igb.visualization.util;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+import javafx.application.Platform;
+
+/**
+ *
+ * @author dcnorris
+ */
+public class FXUtilities {
+
+    public static void runAndWait(Runnable runnable) throws InterruptedException, ExecutionException {
+        FutureTask<Void> future = new FutureTask<>(runnable, null);
+        Platform.runLater(future);
+        future.get();
+    }
+
+}
