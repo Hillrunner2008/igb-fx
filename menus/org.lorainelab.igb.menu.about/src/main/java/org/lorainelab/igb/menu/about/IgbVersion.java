b62cfbb74ff4f94c82bc6c48bd41f279d1ff1224
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-08-10 11:30:23 -0400

    added IgbVersion class

---
+++ menus/org.lorainelab.igb.menu.about/src/main/java/org/lorainelab/igb/menu/about/IgbVersion.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.lorainelab.igb.menu.about;
+
+/**
+ *
+ * @author Devdatta Kulkarni
+ */
+public class IgbVersion {
+    
+    private final static int MAJOR_VERSION = 9;
+    private final static int MINOR_VERSION = 0;
+    
+    public static String getVersion(){
+        return MAJOR_VERSION+"."+MINOR_VERSION;
+    }
+    
+    public static int getMajorVersion(){
+        return MAJOR_VERSION;
+    }
+    
+    public static int getMinorVersion(){
+        return MINOR_VERSION;
+    }
+    
+}
