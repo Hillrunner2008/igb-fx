3e3bf9eecf43413f1c3e72e180482ae3c7d8293a
David Norris <Hillrunner2008@gmail.com>
2016-08-25 17:06:17 -0400

    added error/warning counts and clear/copy buttons to console tab

--- tabs/org.lorainelab.igb.tabs.console/src/main/java/org/lorainelab/igb/tabs/console/ConsoleTab.java
+++ tabs/org.lorainelab.igb.tabs.console/src/main/java/org/lorainelab/igb/tabs/console/ConsoleTab.java
@@ -6,9 +6,22 @@ import aQute.bnd.annotation.component.Reference;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.SplitPane;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TextArea;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import org.apache.karaf.log.core.LogEventFormatter;
 import org.apache.karaf.log.core.LogService;
@@ -35,12 +48,47 @@ public class ConsoleTab extends Tab implements TabProvider, PaxAppender {
     private static final String INFO = "info";
     private static final String DEBUG = "debug";
     private static final String TRACE = "trace";
+    private IntegerProperty ERROR_COUNTER;
+    private IntegerProperty WARN_COUNTER;
+    private Button copyBtn;
+    private Button clearBtn;
 
     public ConsoleTab() {
         consoleTextArea = new TextArea();
+        consoleTextArea.setEditable(false);
+        ERROR_COUNTER = new SimpleIntegerProperty();
+        WARN_COUNTER = new SimpleIntegerProperty();
+        clearBtn = new Button("Clear");
+        copyBtn = new Button("Copy");
+        initializeGuiComponents();
         setupLoggingRedirects();
-        StackPane pane = new StackPane(consoleTextArea);
-        this.setContent(pane);
+        initializeBtnActions();
+    }
+
+    private void initializeGuiComponents() {
+        Label errorCount = new Label();
+        Label warnCount = new Label();
+        StringProperty errorCountPrefix = new SimpleStringProperty("Error Count: ");
+        StringProperty warnCountPrefix = new SimpleStringProperty("Warning Count: ");
+        errorCount.textProperty().bind(errorCountPrefix.concat(ERROR_COUNTER.asString()));
+        warnCount.textProperty().bind(warnCountPrefix.concat(WARN_COUNTER.asString()));
+        StackPane leftSide = new StackPane(consoleTextArea);
+        SplitPane split = new SplitPane();
+        split.setDividerPositions(.90);
+        clearBtn.setMaxWidth(Double.MAX_VALUE);
+        copyBtn.setMaxWidth(Double.MAX_VALUE);
+        VBox vbButtons = new VBox();
+        vbButtons.setSpacing(10);
+        vbButtons.setPadding(new Insets(0, 20, 10, 20));
+        vbButtons.getChildren().addAll(clearBtn, copyBtn);
+        Pane spacer = new Pane();
+        VBox.setVgrow(spacer, Priority.ALWAYS);
+        VBox rightSide = new VBox(errorCount, warnCount, spacer, vbButtons);
+        rightSide.setPadding(new Insets(5, 20, 10, 20));
+        split.getItems().add(leftSide);
+        split.getItems().add(rightSide);
+        SplitPane.setResizableWithParent(split, Boolean.FALSE);
+        this.setContent(split);
     }
 
     private void setupLoggingRedirects() {
@@ -77,7 +125,12 @@ public class ConsoleTab extends Tab implements TabProvider, PaxAppender {
 
     @Override
     public void doAppend(PaxLoggingEvent event) {
-//        String lvl = event.getLevel().toString().toLowerCase();
+        String lvl = event.getLevel().toString().toLowerCase();
+        if (FATAL.equals(lvl) || ERROR.equals(lvl)) {
+            ERROR_COUNTER.setValue(ERROR_COUNTER.add(1).intValue());
+        } else if (WARN.equals(lvl)) {
+            WARN_COUNTER.setValue(WARN_COUNTER.add(1).intValue());
+        }
         final String logLine = eventFormatter.format(event, null, true);
         Platform.runLater(() -> {
             consoleTextArea.selectEnd();
@@ -118,6 +171,18 @@ public class ConsoleTab extends Tab implements TabProvider, PaxAppender {
     private String colorToHex(Color color) {
         String hex2 = "#" + Integer.toHexString(color.hashCode());
         return hex2;
+    }
+
+    private void initializeBtnActions() {
+        clearBtn.setOnAction(action -> {
+            consoleTextArea.clear();
+        });
+        copyBtn.setOnAction(action -> {
+            final Clipboard clipboard = Clipboard.getSystemClipboard();
+            final ClipboardContent content = new ClipboardContent();
+            content.putString(consoleTextArea.getText());
+            clipboard.setContent(content);
+        });
     }
 
 }
