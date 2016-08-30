3a711d8825d5b1021d2a6e5e429aca35dd7df986
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2016-08-30 11:35:55 -0400

    added option to delete all preferences and close appplication

--- org.lorainelab.igb.preferences.otheroptions/src/main/java/org/lorainelab/igb/preferences/otheroptions/OtherPreferences.java
+++ org.lorainelab.igb.preferences.otheroptions/src/main/java/org/lorainelab/igb/preferences/otheroptions/OtherPreferences.java
@@ -7,28 +7,59 @@ package org.lorainelab.igb.preferences.otheroptions;
 
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import java.util.Optional;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Tab;
 import org.lorainelab.igb.preferencemanager.api.PreferencesTabProvider;
+import org.lorainelab.igb.preferences.PreferenceUtils;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author Devdatta Kulkarni
  */
 @Component(immediate = true)
-public class OtherPreferences implements PreferencesTabProvider{
- 
+public class OtherPreferences implements PreferencesTabProvider {
+
+    Button resetButton;
+    Tab otherPrefTab;
+    private static final org.slf4j.Logger LOG = LoggerFactory.getLogger(OtherPreferences.class);
+
     @Activate
     public void activate() {
+        resetButton = new Button("Reset Preferences");
+        resetButton.setOnAction(ae -> {
+            Platform.runLater(() -> {
+                Alert alert = new Alert(AlertType.CONFIRMATION);
+                alert.setTitle("Preferences reset confirmation");
+                alert.setHeaderText("Reset all preferences to default");
+                alert.setContentText("Preferences reset needs application restart.\nAre you sure want reset preferences and exit?");
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == ButtonType.OK) {
+                    LOG.info("Reset preferences requested.");
+                    PreferenceUtils.clearAllPreferences();
+                    LOG.debug("Preferences cleared, terminating the application");
+                    System.exit(0);
+                }
+
+            });
+        });
+        otherPrefTab = new Tab("Other Options");
+        otherPrefTab.setContent(resetButton);
     }
-    
+
     @Override
     public Tab getPreferencesTab() {
-        return new Tab("Other options");
+        return otherPrefTab;
     }
 
     @Override
     public int getTabWeight() {
         return 100;
     }
-    
+
 }
