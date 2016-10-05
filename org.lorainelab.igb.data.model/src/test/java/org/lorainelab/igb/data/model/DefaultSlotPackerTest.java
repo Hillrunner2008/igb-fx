7f555daf55e1a2a37146771b1d9052d4f6f127af
David Norris <Hillrunner2008@gmail.com>
2016-10-05 19:12:57 -0400

    increased space allocation for labels

--- org.lorainelab.igb.data.model/src/test/java/org/lorainelab/igb/data/model/DefaultSlotPackerTest.java
+++ org.lorainelab.igb.data.model/src/test/java/org/lorainelab/igb/data/model/DefaultSlotPackerTest.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import static org.lorainelab.igb.data.model.DefaultSlotPacker.packBottomToTop;
 import static org.lorainelab.igb.data.model.DefaultSlotPacker.packTopToBottom;
+import static org.lorainelab.igb.data.model.glyph.Glyph.SLOT_HEIGHT;
 
 /**
  *
@@ -21,19 +22,20 @@ public class DefaultSlotPackerTest {
         double minY = packTopToBottom(0);
         assertEquals(minY, 0, 0);
         minY = packTopToBottom(1);
-        assertEquals(minY, 30, 0);
+        assertEquals(minY, SLOT_HEIGHT, 0);
         minY = packTopToBottom(2);
-        assertEquals(minY, 60, 0);
+        assertEquals(minY, SLOT_HEIGHT * 2, 0);
     }
 
     @Test
     public void testPackBottomToTop() {
-        double minY = packBottomToTop(0, 100);
-        assertEquals(minY, 70, 0);
-        minY = packBottomToTop(1, 100);
-        assertEquals(minY, 40, 0);
-        minY = packBottomToTop(2, 100);
-        assertEquals(minY, 10, 0);
+        final int height = 100;
+        double minY = packBottomToTop(0, height);
+        assertEquals(minY, height - SLOT_HEIGHT, 0);
+        minY = packBottomToTop(1, height);
+        assertEquals(minY, height - (SLOT_HEIGHT * 2), 0);
+        minY = packBottomToTop(2, height);
+        assertEquals(minY,  height - (SLOT_HEIGHT * 3), 0);
     }
 
 }
