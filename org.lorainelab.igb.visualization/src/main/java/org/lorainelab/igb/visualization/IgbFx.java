43cb7f3d40c8e71e563e744ae7fdf81f28206e73
Devdatta Kulkarni <devd.kulkarni@gmail.com>
2017-01-26 16:19:56 -0500

    dropdown removed and title changed

--- org.lorainelab.igb.visualization/src/main/java/org/lorainelab/igb/visualization/IgbFx.java
+++ org.lorainelab.igb.visualization/src/main/java/org/lorainelab/igb/visualization/IgbFx.java
@@ -4,13 +4,21 @@ import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
 import aQute.bnd.annotation.component.Deactivate;
 import aQute.bnd.annotation.component.Reference;
+import java.util.Optional;
 import javafx.animation.PauseTransition;
 import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.beans.value.WeakChangeListener;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.lorainelab.igb.data.model.GenomeVersion;
+import org.lorainelab.igb.data.model.GenomeVersionRegistry;
+import org.lorainelab.igb.selections.SelectionInfoService;
 import org.lorainelab.igb.stage.provider.api.StageProvider;
 import static org.lorainelab.igb.utils.FXUtilities.runAndWait;
+import org.lorainelab.igb.version.IgbVersion;
 import org.lorainelab.igb.visualization.ui.Root;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
@@ -23,6 +31,8 @@ public class IgbFx {
     private Stage stage;
     private Root root;
     private StageProvider stageProvider;
+    private SelectionInfoService selectionInfoService;
+    private ChangeListener<Optional<?>> selectionChangeListner;
 
     @Activate
     public void activate(BundleContext bc) {
@@ -42,8 +52,20 @@ public class IgbFx {
 //            } catch (MalformedURLException ex) {
 //                LOG.error(ex.getMessage(), ex);
 //            }
+
                 stage.setTitle("IGBfx");
                 stage.setScene(scene);
+                selectionChangeListner = new ChangeListener<Optional<?>>() {
+                    @Override
+                    public void changed(ObservableValue<? extends Optional<?>> observable, Optional<?> oldValue, Optional<?> newValue) {
+                        String[] title = {"Integrated Genome Browser FX " + IgbVersion.getVersion()};
+                        selectionInfoService.getSelectedGenomeVersion().get().ifPresent(genome -> title[0] = "(" + genome.getSpeciesName().get() + ") - " + title[0]);
+                        selectionInfoService.getSelectedChromosome().get().ifPresent(chromosome -> title[0] = chromosome.getName() + title[0]);
+                        Platform.runLater(() -> stage.setTitle(title[0]));
+                    }
+                };
+                selectionInfoService.getSelectedChromosome().addListener(new WeakChangeListener<>(selectionChangeListner));
+                selectionInfoService.getSelectedGenomeVersion().addListener(new WeakChangeListener<>(selectionChangeListner));
 
             });
             PauseTransition delay = new PauseTransition(Duration.seconds(1.5));
@@ -78,6 +100,11 @@ public class IgbFx {
     @Reference
     public void setRoot(Root root) {
         this.root = root;
+    }
+
+    @Reference
+    public void setGenomeVersionRegistry(SelectionInfoService selectionInfoService) {
+        this.selectionInfoService = selectionInfoService;
     }
 
 }
